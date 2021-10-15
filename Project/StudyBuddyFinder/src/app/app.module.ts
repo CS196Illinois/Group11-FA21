1625565638e380556958bb9157c3224ca7465870
Shreya Deshpande <shreyadeshpande@wirelessprv-10-195-179-99.near.illinois.edu>
2021-10-15 15:31:43 -0500

    we added a component for the homepage and header, and added a few buttons in navbar

--- Project/StudyBuddyFinder/src/app/app.module.ts
+++ Project/StudyBuddyFinder/src/app/app.module.ts
@@ -2,15 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { HeaderComponent } from './homepage/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, HomepageComponent, HeaderComponent]
 })
 export class AppModule { }
