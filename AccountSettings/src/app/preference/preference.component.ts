ae561bd0ef84f3266ae4e7bf53b10685cc8ef3f5
sahithya <sr55@illinois.edu>
2021-10-25 15:50:58 -0500

    started on settings

--- AccountSettings/src/app/preference/preference.component.ts
+++ AccountSettings/src/app/preference/preference.component.ts
@@ -5,5 +5,7 @@ import { Component } from '@angular/core';
     templateUrl: './preference.component.html'
 })
 export class PreferenceComponent {
-
+    //name: type = value;
+    preferenceID = 10;
+    preferenceStatus = 'offline';
 }
