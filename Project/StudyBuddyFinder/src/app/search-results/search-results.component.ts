da94649e0208e50e017614f7962436d36dd28d7a
shreyad216 <90356482+shreyad216@users.noreply.github.com>
2021-10-29 17:26:46 -0500

    added buttons on search results page with dummy array

--- Project/StudyBuddyFinder/src/app/search-results/search-results.component.ts
+++ Project/StudyBuddyFinder/src/app/search-results/search-results.component.ts
@@ -6,7 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-results.component.css']
 })
 export class SearchResultsComponent implements OnInit {
-
+  people: String[] = [
+    "Sailaja",
+    " Shreya",
+    " Dimple",
+    " Ishq",
+    " Sahithya"
+  ];
   constructor() { }
 
   ngOnInit(): void {
