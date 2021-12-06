9e5d09a8ae67029de2c1106f2ed042c623bf895c
Ishq <ig8@illinois.edu>
2021-12-05 21:48:15 -0600

    committing latest changes

--- Project/StudyBuddyFinder/src/app/app-routing.module.ts
+++ Project/StudyBuddyFinder/src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
 import { SearchResultsComponent } from './search-results/search-results.component';
 import { UserprofileComponent } from './userprofile/userprofile.component';
+import { SettingsComponent } from './settings/settings.component';
 
 const routes: Routes = [
     { path: '', component: HomepageComponent },
     { path: 'search-results/:className', component: SearchResultsComponent },
-    { path: 'user/:username', component: UserprofileComponent}
+    { path: 'user/:username', component: UserprofileComponent},
+    { path: 'settings', component: SettingsComponent}
 ]
 
 
